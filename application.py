88911fac546b5614f3bc0fe247a31014a531fac8
yashkasturi <37289085+yashkasturi@users.noreply.github.com>
2019-04-16 00:54:15 +0530

    Update application.py

--- application.py
+++ application.py
@@ -10,24 +10,6 @@ from skimage.measure import compare_ssim
 
 app = Flask(__name__)
 
-@app.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
-
-@app.after_request
-def add_header(response):
-    response.cache_control.max_age = 0
-    response.cache_control.public = True
-    return response
-
 @app.route('/')
 def css():
     return render_template('mangrove_Index.html')
