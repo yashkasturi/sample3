b60ee86007b442d6540f4f517cf17cff5a91e55c
yashkasturi <37289085+yashkasturi@users.noreply.github.com>
2019-04-16 00:59:36 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,12 +1,4 @@
-import cv2
-import imutils
-import numpy
-import requests
 from flask import Flask, render_template, request
-from flask_mysqldb import MySQL
-from PIL import Image
-from skimage.io import imread
-from skimage.measure import compare_ssim
 
 app = Flask(__name__)
 
