d9e524f2f7769b7e4f6654bfe00f80ec7f1b8b40
yashkasturi <37289085+yashkasturi@users.noreply.github.com>
2019-04-17 16:20:19 +0530

    Update application.py

--- application.py
+++ application.py
@@ -2,31 +2,12 @@ from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 from PIL import Image
 import requests
-
 from skimage.measure import compare_ssim
 from skimage.io import imread
 import imutils
 import sys
 
 app = Flask(__name__)
-
-@app.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
-
-@app.after_request
-def add_header(response):
-    response.cache_control.max_age = 0
-    response.cache_control.public = True
-    return response
 
 @app.route('/')
 def page1():
