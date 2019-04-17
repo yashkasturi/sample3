fa346cf01dbd3817768076cb90408afcad93c9a3
yashkasturi <37289085+yashkasturi@users.noreply.github.com>
2019-04-17 10:05:06 +0530

    Update application.py

--- application.py
+++ application.py
@@ -6,6 +6,24 @@ import imgsave
 
 app = Flask(__name__)
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+@app.after_request
+def add_header(response):
+    response.cache_control.max_age = 0
+    response.cache_control.public = True
+    return response
+
 @app.route('/')
 def page1():
     return render_template('mangrove_Index.html')
